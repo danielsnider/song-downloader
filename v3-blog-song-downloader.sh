f6642d12de276f907a666bf0e3f9e7cd0abae7b1
Daniel Snider <danielsnider12@gmail.com>
2012-11-04 20:47:59 +0100

    added license

--- v3-blog-song-downloader.sh
+++ v3-blog-song-downloader.sh
@@ -1,8 +1,23 @@
 #!/bin/bash
-#this filse downloads mp3 song links from a website
-#this is coded with files instead of variables because variables strip \n's and that causes problems
-#Inputs: url of site with .mp3s, output path for files
-#note to self you may want to save at path 
+# 
+# Copyright 2010 Daniel Snider
+#
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+
+# You should have received a copy of the GNU General Public License
+# along with this program.  If not, see <http://www.gnu.org/licenses/>
+
+"""	This program downloads mp3 song links from a website
+	Inputs: url of website with .mp3s, output path for files
+"""
 
 url=$1  
 html_file="page.html"
